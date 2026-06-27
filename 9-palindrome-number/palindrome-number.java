f208b72d8b455b9a7b951fcd493a4620d9294adf
Aditya Singh <aditya.singh7_cs23@gla.ac.in>
2026-06-27 21:50:02 +0530

    Time: 5 ms (79.49%) | Memory: 45.8 MB (86.66%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+    int rev = 0;
+    int orig = x;
+
+    while (x > 0) {
+        int lastDigit = x % 10;
+        rev = (rev * 10) + lastDigit;
+        x = x / 10;
+    }
+    return orig == rev;
+    }
+}
